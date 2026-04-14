99085525461bf796f3da8c465d98dc11d4f056be
anaghabide <42738725+anaghabide@users.noreply.github.com>
2026-04-14 17:34:04 +0530

    Add: house-robber-ii - submission-1

---
+++ Data Structures & Algorithms/house-robber-ii/submission-1.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int rob(int[] nums) {
+        if(nums.length==0) return 0;
+        if(nums.length==1) return nums[0];
+      return Math.max(helper(nums, 0, nums.length-2),
+                    helper(nums, 1, nums.length-1));
+    }
+
+    public int helper(int nums[], int start, int end){
+        int prev1=0;
+        int prev2=0;
+        for(int i=start; i<=end; i++){
+            int curr = Math.max(nums[i] + prev2, prev1);
+            prev2 = prev1;
+            prev1 = curr;
+        }
+        return prev1;
+    }
+}
