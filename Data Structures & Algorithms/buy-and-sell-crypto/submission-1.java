f4654c83205863a544bd5115121b4f78e1aabcab
anaghabide <42738725+anaghabide@users.noreply.github.com>
2026-04-14 18:35:38 +0530

    Add: buy-and-sell-crypto - submission-1

---
+++ Data Structures & Algorithms/buy-and-sell-crypto/submission-1.java
@@ -0,0 +1,29 @@
+class Solution {
+    public int maxProfit(int[] prices) {
+       int profit = 0;
+       int minPrice = Integer.MAX_VALUE;
+       int buy_day = 0, sell_day = 0, buy_price = 0, sell_price = 0;
+       int min_day=0;
+       for(int i=0; i<prices.length; i++){
+        int currPrice= prices[i];
+        if(currPrice<minPrice){
+            minPrice = currPrice; //buy day
+            min_day = i;
+        }else{
+            //profit = Math.max(profit, currPrice-minPrice);
+             if (prices[i] - minPrice > profit) {
+                profit = prices[i] - minPrice;
+                sell_price = currPrice;
+                buy_price = minPrice;
+                sell_day = i;
+                buy_day = min_day;
+             }
+        }
+       }
+       System.out.println("buy day :: " + buy_day);
+       System.out.println("buy price :: " + buy_price);
+       System.out.println("sell day :: " + sell_day);
+       System.out.println("sell price :: " + sell_price);
+       return profit;
+    }
+}
